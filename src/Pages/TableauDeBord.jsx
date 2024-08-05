797518b9e5bb3a36092711618139d5080f004bd2
Rida Ouajib <rida.ouajib@outlook.fr>
2024-08-05 16:21:34 +0200

    Ajout de la fonctionnalité modification dans le tableau de bord

--- src/Pages/TableauDeBord.jsx
+++ src/Pages/TableauDeBord.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import NavBar from '../Components/NavBar';
 import '../Styles/TableauDeBord.css';
 import Footer from '../Components/Footer';
@@ -8,179 +8,167 @@ import EntrepriseService from '../Services/EntrepriseService';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 
-
-
 const TableauDeBord = () => {
-
     const [administrateurs, setAdministrateurs] = useState({});
     const [entreprises, setEntreprises] = useState([]);
-    const [entreprisesID, setEntreprisesID] = useState ([]);
     const [nouvelleEntreprise, setNouvelleEntreprise] = useState({
-        ent_nom : "",
-        ent_mdp : "",
-        ent_date_inscription : "",
-        ent_logo_url : "" 
-    }); 
+        ent_nom: "",
+        ent_mdp: "",
+        ent_date_inscription: "",
+        ent_logo_url: ""
+    });
+    const [entrepriseAModifier, setEntrepriseAModifier] = useState({
+        ent_id: "",
+        ent_nom: "",
+        ent_mdp: "",
+        ent_date_inscription: "",
+        ent_logo_url: ""
+    });
     const token = localStorage.getItem("token");
-    const decoded = jwtDecode(token); /* decodé le token pour obtenir l'ID  */ 
+    const decoded = jwtDecode(token);
     const navigate = useNavigate();
 
-    /* console.log(decoded); */
-
-      // handle click pour l'évenement de la déconnexion avec le button
-  const handleLogout = () => {
-    // remove the token and user from the session storage
-    sessionStorage.removeItem("token");
-    sessionStorage.removeItem('email');
-    navigate('/connexion-admin');
-    toast.success("Déconnexion avec succès");
-  }
+    const handleLogout = () => {
+        sessionStorage.removeItem("token");
+        sessionStorage.removeItem('email');
+        navigate('/connexion-admin');
+        toast.success("Déconnexion avec succès");
+    };
 
     const fetchAdministrateursByID = async () => {
-        const data = await AdminService.getAdministrateurByID(decoded.id).then((response) => {
-            console.log(response.data);
-            setAdministrateurs(response.data);
-        }) 
-
-    }
-    
-    const removeEntreprise = async (id) => {
-        try {
-            const response = await EntrepriseService.removeEntreprise(id)
-            fetchEntreprises()
-            toast.success("Entreprise supprimée")
-        } catch (error) {
-            toast.error(error.response.data.error)
-            console.log(error);
-            
-        }
-
-    }
-
-    console.log(administrateurs);
-
+        const data = await AdminService.getAdministrateurByID(decoded.id);
+        setAdministrateurs(data.data);
+    };
 
     const fetchEntreprises = async () => {
-        const response = await EntrepriseService.getAllEntreprise().then((response) => {
-            console.log(response.data);
-            setEntreprises(response.data);
-        });
-
-    console.log(entreprises);
-            
-    }
-
+        const response = await EntrepriseService.getAllEntreprise();
+        setEntreprises(response.data);
+    };
 
     const addEntreprises = async () => {
         try {
-            const response = await EntrepriseService.addEntreprise(nouvelleEntreprise);
+            await EntrepriseService.addEntreprise(nouvelleEntreprise);
             toast.success("Entreprise ajoutée");
-            fetchEntreprises()
+            fetchEntreprises();
         } catch (error) {
-            toast.error(error.response.data.error)
-            console.log(error);
+            toast.error(error.response.data.error);
         }
-    }
+    };
 
-    const updateEntreprise = async (id) => {
+    const updateEntreprise = async () => {
         try {
-            const reponse = await EntrepriseService.updateEntreprise()
-            toast.success("Modication réalisée")
+            await EntrepriseService.updateEntreprise(entrepriseAModifier.ent_id, {
+                ent_nom: entrepriseAModifier.ent_nom,
+                ent_mdp: entrepriseAModifier.ent_mdp,
+                ent_date_inscription: entrepriseAModifier.ent_date_inscription,
+                ent_logo_url: entrepriseAModifier.ent_logo_url
+            });
+            toast.success("Modification réalisée");
+            fetchEntreprises();
         } catch (error) {
-            toast.error(error.response.data.error)
-            console.log(error);
+            toast.error(error.response.data.error);
         }
+    };
+
+
+const removeEntreprise = async (id) => {
+    try {
+        const response = await EntrepriseService.removeEntreprise(id)
+        fetchEntreprises()
+        toast.success("Entreprise supprimée")
+    } catch (error) {
+        toast.error(error.response.data.error)
+        console.log(error);
+        
     }
 
-    useEffect (() => {
-        fetchAdministrateursByID();    /* Que des fetch la plupart du temps dans les useEffects grossomodo */
+}
+ 
+    useEffect(() => {
+        fetchAdministrateursByID();
         fetchEntreprises();
-
     }, []);
 
-
-    return <> 
-        <NavBar/>
-        <div id='status'>
-            <p id='etat'>Administrateur</p>
-            <button id='logout' onClick={handleLogout}>Déconnexion</button>
-        </div>
-        <div id='titre-tableaudebord'>
-            <h1>Tableau de bord</h1>
-            <h2> {administrateurs?.adm_nom} </h2> {/* le point d'interrogation signifie que si la clé de l'objet n'existe pas, ça ne cassera pas */}
-        </div>
-        <div id='jumbotron'>
+    return (
+        <>
+            <NavBar />
+            <div id='status'>
+                <p id='etat'>Administrateur</p>
+                <button id='logout' onClick={handleLogout}>Déconnexion</button>
+            </div>
+            <div id='titre-tableaudebord'>
+                <h1>Tableau de bord</h1>
+                <h2>{administrateurs?.adm_nom}</h2>
+            </div>
+            <div id='jumbotron'>
                 <div className='block-conteneur-crud'>
                     <a href='#ajoutentreprise' target='self_' ><h3>Ajouter une entreprise cliente</h3></a>
                 </div>
-        </div>
-        <div id='block-nb-entreprise'>
-            <h3>Nombre d'entreprise : {entreprises.length}
-            </h3>
-        </div>
-        <div id='block-conteneur-formulaire'>
-            <div className='block-crud-formulaire' >
-                <h4 id='ajoutentreprise'>Ajouter une entreprise</h4>
-                <label className="designation-champ" htmlFor="">NOM :</label>
-                <input type="text" required onChange={(e) => {setNouvelleEntreprise({ ...nouvelleEntreprise, ent_nom : e.target.value })}} />
-                <label className="designation-champ" htmlFor="">MOT DE PASSE :</label> 
-                <input type="password" required onChange={(e) => {setNouvelleEntreprise({ ...nouvelleEntreprise, ent_mdp : e.target.value})}}/>
-                <label className="designation-champ" htmlFor="">DATE D'INSCRIPTION</label>
-                <input type="date" required onChange={(e) => {setNouvelleEntreprise ({ ...nouvelleEntreprise, ent_date_inscription : e.target.value})}} />
-                <label className="designation-champ" htmlFor="">URL LOGO</label>
-                <input type="text"  required onChange={(e) => {setNouvelleEntreprise ({ ...nouvelleEntreprise, ent_logo_url : e.target.value})}} />
-                <label className="designation-champ" htmlFor=""></label>
-                <input className='button-formulaire' type="submit" value="Valider" onClick={() => { addEntreprises()}} />
             </div>
-            <div className='block-crud-formulaire' >
-                <h4 id='modifierentreprise'>Modifier / Remplacer une entreprise cliente</h4>
-                <label className="designation-champ" htmlFor="">NOM :</label>
-                <input type="text" />
-                <label className="designation-champ" htmlFor="">MOT DE PASSE :</label> 
-                <input type="password" name="" id="" />
-                <label className="designation-champ" htmlFor="">URL LOGO</label>
-                <input type="text" name="" id="" />
-                <label className="designation-champ" htmlFor=""></label>
-                <input className='button-formulaire' type="submit" value="Valider" />
+            <div id='block-nb-entreprise'>
+                <h3>Nombre d'entreprise : {entreprises.length}</h3>
             </div>
-        </div>
-        <div id='tableau-gestion'>
-            <table >
-                <thead>
-                <tr>
-                    <th>ID Entreprise</th>
-                    <th>Nom</th>
-                    <th>Date d'inscription</th>
-                    <th>LOGO</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                {entreprises.map(entreprise => (
-                    <tr key={entreprise.ent_id}>
-                    <td>
-                        {entreprise.ent_id}
-                    </td>
-                    <td>
-                        {entreprise.ent_nom}
-                    </td>
-                    <td>
-                        {entreprise.ent_date_inscription}
-                    </td>
-                    <td>
-                        <img id='logo-entreprise' src={entreprise.ent_logo_url} alt="logo_entreprise" height={80} width={85}/>
-                    </td>
-                    <td>
-                        <button className='action'>MODIFIER</button>
-                        <button className='action' onClick={() => removeEntreprise(entreprise.ent_id)}>SUPPRIMER</button>
-                    </td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-        <Footer/>
-    </>
-}
+            <div id='block-conteneur-formulaire'>
+                <div className='block-crud-formulaire'>
+                    <h4 id='ajoutentreprise'>Ajouter une entreprise</h4>
+                    <label className="designation-champ" htmlFor="">NOM :</label>
+                    <input type="text" required onChange={(e) => setNouvelleEntreprise({ ...nouvelleEntreprise, ent_nom: e.target.value })} />
+                    <label className="designation-champ" htmlFor="">MOT DE PASSE :</label>
+                    <input type="password" required onChange={(e) => setNouvelleEntreprise({ ...nouvelleEntreprise, ent_mdp: e.target.value })} />
+                    <label className="designation-champ" htmlFor="">DATE D'INSCRIPTION</label>
+                    <input type="date" required onChange={(e) => setNouvelleEntreprise({ ...nouvelleEntreprise, ent_date_inscription: e.target.value })} />
+                    <label className="designation-champ" htmlFor="">URL LOGO</label>
+                    <input type="text" required onChange={(e) => setNouvelleEntreprise({ ...nouvelleEntreprise, ent_logo_url: e.target.value })} />
+                    <label className="designation-champ" htmlFor=""></label>
+                    <input className='button-formulaire' type="submit" value="Valider" onClick={addEntreprises} />
+                </div>
+  
+                <div className='block-crud-formulaire'>
+                    <h4 id='modifierentreprise'>Modifier / Remplacer une entreprise cliente</h4>
+                    <label className="designation-champ" htmlFor="">NOM :</label>
+                    <input type="text" name="ent_id" value={entrepriseAModifier.ent_nom} onChange={(e) => {setEntrepriseAModifier({...entrepriseAModifier, [e.target.name] : e.target.value}) }} />
+                    <label className="designation-champ" htmlFor="">MOT DE PASSE :</label>
+                    <input type="password" name="ent_nom" value={entrepriseAModifier.ent_mdp} onChange={(e) => {setEntrepriseAModifier({...entrepriseAModifier, [e.target.name] : e.target.value}) }}  />
+                    <label className="designation-champ" htmlFor="">DATE D'INSCRIPTION :</label>
+                    <input type="date" name="ent_mdp" value={entrepriseAModifier.ent_date_inscription} onChange={(e) => {setEntrepriseAModifier({...entrepriseAModifier, [e.target.name] : e.target.value}) }}  />
+                    <label className="designation-champ" htmlFor="">URL LOGO</label>
+                    <input type="text" name="ent_logo_url" value={entrepriseAModifier.ent_logo_url} onChange={(e) => {setEntrepriseAModifier({...entrepriseAModifier, [e.target.name] : e.target.value}) }}  />
+                    <label className="designation-champ" htmlFor=""></label>
+                    <input className='button-formulaire' type="submit" value="Valider" onClick={updateEntreprise} />
+                </div>
+            </div>
+            <div id='tableau-gestion'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID Entreprise</th>
+                            <th>Nom</th>
+                            <th>Date d'inscription</th>
+                            <th>LOGO</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {entreprises.map(entreprise => (
+                            <tr key={entreprise.ent_id}>
+                                <td>{entreprise.ent_id}</td>
+                                <td>{entreprise.ent_nom}</td>
+                                <td>{entreprise.ent_date_inscription}</td>
+                                <td>
+                                    <img id='logo-entreprise' src={entreprise.ent_logo_url} alt="logo_entreprise" height={80} width={85} />
+                                </td>
+                                <td>
+                                   <a href="#modifierentreprise"><button className='action' onClick={() => setEntrepriseAModifier(entreprise)}>MODIFIER</button></a> 
+                                    <button className='action' onClick={() => removeEntreprise(entreprise.ent_id)}>SUPPRIMER</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <Footer />
+        </>
+    );
+};
 
-export default TableauDeBord ;
+export default TableauDeBord;
--- src/Services/EntrepriseService.js
+++ src/Services/EntrepriseService.js
@@ -1,27 +1,26 @@
 import axios from "axios";
 import instance from "../API/Axios";
 
-    const token = localStorage.getItem("token")
+const token = localStorage.getItem("token");
 
-    const getAllEntreprise = () => {
-        return instance(token).get("/entreprises");
-    }
+const getAllEntreprise = () => {
+    return instance(token).get("/entreprises");
+};
 
-    const getEntrepriseByID = (id) => {
-        return instance(token).get("/entreprises/" + id);
-    }
+const getEntrepriseByID = (id) => {
+    return instance(token).get("/entreprises/" + id);
+};
 
-    const addEntreprise = (data) => {
-        return instance(token).post("/entreprises", data)
-    }
+const addEntreprise = (data) => {
+    return instance(token).post("/entreprises", data);
+};
 
-    const removeEntreprise = (id) => {
-        return instance(token).delete("/entreprises/" + id);
-    }
+const removeEntreprise = (id) => {
+    return instance(token).delete("/entreprises/" + id);
+};
 
-    const updateEntreprise = (id, data) => {
-        return instance(token).patch("/entreprises/" + id, data)
-    }
+const updateEntreprise = (id, data) => {
+    return instance(token).patch("/entreprises/" + id, data);
+};
 
-
-export default {getAllEntreprise, getEntrepriseByID, addEntreprise, removeEntreprise, updateEntreprise} ;
+export default { getAllEntreprise, getEntrepriseByID, addEntreprise, removeEntreprise, updateEntreprise };
